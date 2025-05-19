24ec5bba349a83dc34899a645d2ce601ea6132ee
Hala Alshareef <alshareefhala10@gmail.com>
2025-05-19 01:11:20 -0400

    bugs

--- components/BitcoinModel.tsx
+++ components/BitcoinModel.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Environment, useGLTF } from '@react-three/drei';
 import { motion } from 'framer-motion';
+import * as THREE from 'three';
+
 
 function Model({ spinning = true }) {
   const modelRef = useRef<THREE.Mesh>(null);
